a7a7014f946166b59520bd6cadd63fdbc02b7056
Yooo31 <mrts.yoan@gmail.com>
2023-10-11 00:21:01 +0200

    End of footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
+import { BsGithub, BsLinkedin, BsFillEnvelopeFill } from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer>
       <ul className='flex flex-wrap'>
-        <li className='px-4 ml-5'>GitHub</li>
-        <li className='px-4 ml-5'>LinkedIn</li>
-        <li className='px-4 ml-5'>Mail</li>
+        <li className='px-4 ml-5'><Link target="_blank" href="https://github.com/Yooo31/"><BsGithub cursor="pointer" size={25}/></Link></li>
+        <li className='px-4 ml-5'><Link target="_blank" href="https://www.linkedin.com/in/yoan-martins/"><BsLinkedin cursor="pointer" size={25}/></Link></li>
+        <li className='px-4 ml-5'><Link target="_blank" href="mailto:mrts.yoan@gmail.com"><BsFillEnvelopeFill cursor="pointer" size={25}/></Link></li>
       </ul>
     </footer>
   )
