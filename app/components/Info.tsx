8c726f1c548e8ce36e22ba606f25660ed4f17a71
SeniorYoan <yoan@kipsoft.fr>
2023-10-10 12:57:51 +0200

    Update css

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -3,15 +3,7 @@ import React from 'react'
 const Info = () => {
   return (
     <>
-      <div className='w-3/5'>
-        <h2>Hello Worls !!</h2>
-      </div>
-      <div className='w-1/5'>
-        <svg className='w-220' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path fill="#FF0066" d="M46,-74C58.8,-72.2,68,-58.4,69,-44C69.9,-29.7,62.7,-14.9,63.6,0.5C64.5,15.9,73.6,31.8,70.4,42.2C67.2,52.7,51.6,57.5,37.8,63.4C24,69.3,12,76.2,0.9,74.7C-10.2,73.1,-20.5,63.1,-31.4,55.6C-42.3,48.1,-53.9,42.9,-56.9,34.1C-59.9,25.2,-54.2,12.6,-50.9,1.9C-47.6,-8.8,-46.7,-17.6,-42.9,-25C-39,-32.3,-32.2,-38.1,-24.5,-42.9C-16.9,-47.8,-8.4,-51.7,4.1,-58.7C16.5,-65.7,33.1,-75.8,46,-74Z" transform="translate(100 100)" />
-        </svg>
-      </div>
-      {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error provident illo magnam autem mollitia debitis odit facere eius. Autem aspernatur perferendis, accusamus cum accusantium ad maiores! Soluta eum quos nam repudiandae dolorem excepturi, autem sit dolore. Labore, dicta harum nam vel accusamus voluptatem ut voluptates porro earum, exercitationem ipsam nisi et, consectetur illum praesentium possimus commodi quas. Pariatur illum unde tempore incidunt at natus aliquid eos consequatur placeat, eius praesentium maxime possimus esse quo vel saepe fugit perferendis dignissimos facere qui dolorum. Nam, iure veritatis nulla expedita porro dicta accusamus, officiis quaerat omnis illo eligendi. Animi itaque dolorum illum in aliquam. Sed praesentium quae cupiditate animi suscipit facilis beatae ipsa consectetur atque est ad, sapiente eum quas autem ullam obcaecati nihil, reprehenderit quasi hic dolores nostrum voluptatem laboriosam. Exercitationem molestiae et dolorum eos officiis iusto deleniti beatae voluptatum necessitatibus facilis quis, mollitia, veritatis sed accusamus molestias. Minima odit hic ratione architecto explicabo illo, odio voluptate, ducimus, sed ipsum libero eligendi dolorum voluptatem. Eveniet minima minus, sapiente ipsum atque fugit voluptates, obcaecati consequatur enim voluptas exercitationem repudiandae dolor! Quisquam dignissimos fuga at, libero eaque assumenda cum laborum pariatur, quos nulla deserunt saepe nemo sed aperiam! Alias molestias dolore illum nostrum reprehenderit tempore. Asperiores, dolore officiis debitis tempora dolor molestias? In quod laborum, accusantium vero eum impedit asperiores, quibusdam nostrum corporis odit ullam magnam sapiente? Non suscipit, a incidunt consectetur ut saepe sint atque. Iste necessitatibus aliquam, praesentium, fuga quas autem officiis obcaecati doloremque fugit omnis magni perspiciatis? Impedit maiores enim consequatur ipsam nemo accusantium repellat explicabo ullam perspiciatis! Magni aliquid dolore officia at molestiae? Provident, quas. Cum, a quasi tenetur aperiam itaque temporibus ex sequi. Rem eius, nesciunt ipsum omnis quibusdam quis eum enim temporibus saepe minus quaerat recusandae veritatis facilis sint, exercitationem totam consectetur libero commodi, quae tempora? Asperiores, consectetur!</p> */}
+      <h2>Hello world !!!</h2>
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import BottomRight from '@/app/components/BottomRight';
 
 export default function Home() {
   return (
-    <div className="main">
-      <div className="main-container">
-        <div id="main-container-inner" className="main-container-inner">
+    <div className="block box-border fixed top-0 bottom-0 left-0 right-0 overflow-y-auto">
+      <div className="h-full box-border flex justify-center items-start md:h-screen md:max-h-screen md:items-center md:overflow-hidden">
+        <div className="w-screen max-w-[1280px] box-border mt-0 mb-0 p-7 flex relative md:max-h-[790px] md:h-full sm:p-[20px]">
           <TopLeft />
 
-          <div className="inner-container">
+          <div className="w-full h-full">
             <Header />
 
-            <main className="projects-overview"></main>
+            <main className="projects-overview">
+              <Info />
+            </main>
 
             <Footer />
 
