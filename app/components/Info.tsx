6cab2a807bebbec144b014d528bad8fa499d56f2
Yooo31 <mrts.yoan@gmail.com>
2023-10-10 21:33:50 +0200

    Hello world animation

--- app/components/Info.tsx
+++ app/components/Info.tsx
@@ -7,10 +7,14 @@ const Info = () => {
     <>
       <div className="min-h-full flex items-center">
         <div className="hero-content flex-col lg:flex-row-reverse">
-        <Image className="max-w-sm rounded-lg shadow-2xl" src={yoan} alt="Yoan Martins" width={200} height={200} />
+          <Image className="max-w-sm rounded-lg shadow-2xl" src={yoan} alt="Yoan Martins" width={200} height={200} />
 
-          <div>
-            <h1 className="text-5xl font-bold">Hello World !!!</h1>
+          <div className='flex flex-col'>
+            <div className='flex justify-start'>
+              <div>
+                <h1 id="typing" className="text-5xl font-bold overflow-hidden whitespace-nowrap relative">Hello World !!!</h1>
+              </div>
+            </div>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
         </div>
