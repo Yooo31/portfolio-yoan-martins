a7c375fe3072ee07a5bed973980ebbdc805b4739
SeniorYoan <yoan@kipsoft.fr>
2023-10-11 13:00:26 +0200

    Change navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,66 @@
-import React from 'react'
+import Link from "@/node_modules/next/link";
+import React from "react";
 
 const Header = () => {
   return (
     <header>
-      <ul className='flex flex-wrap nav ml-5'>
-        <li className='transition-transform hover:scale-125 hover:border-none px-4 pb-2 active'>Accueil</li>
-        <li className='transition-transform hover:scale-125 hover:border-none px-4 pb-2'>Formation</li>
-        <li className='transition-transform hover:scale-125 hover:border-none px-4 pb-2'>Projets</li>
-        <li className='transition-transform hover:scale-125 hover:border-none px-4 pb-2'>Contact</li>
-      </ul>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link href="/">Accueil</Link>
+              </li>
+              <li>
+                <Link href="/formation">Formation</Link>
+              </li>
+              <li>
+                <Link href="/projets">Projets</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/">Accueil</Link>
+            </li>
+            <li>
+              <Link href="/formation">Formation</Link>
+            </li>
+            <li>
+              <Link href="/projets">Projets</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
