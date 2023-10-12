8c361e7dabc1065c2348caebcc299a9c78bc123f
Yooo31 <mrts.yoan@gmail.com>
2023-10-13 00:40:58 +0200

    Finalisation techno page

--- app/components/Technos.tsx
+++ app/components/Technos.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import Image from 'next/image';
 import LanguagesData from "@/data/languages.json";
 
 const Technos = () => {
@@ -6,13 +6,14 @@ const Technos = () => {
 
   return (
     <>
-    <div className="flex justify-center m-72">
-      { data.map((element, index) => (
-        <div key={index}>
-          <h2>{element.name}</h2>
-          <h2>{element.img}</h2>
-        </div>
-      ))};
+    <div id="languages-list" className="h-5/6 flex justify-center">
+      <div className="flex flex-wrap -mx-4">
+        { data.map((element, index) => (
+          <div key={index} className="w-1/4 px-4 mb-4 flex justify-center">
+            <Image  src={`/images/technos/${element.img}.svg`} alt={element.name} width={150} height={150}/>
+          </div>
+        ))}
+      </div>
     </div>
     </>
   );
