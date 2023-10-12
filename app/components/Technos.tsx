f55769867ca82acee40c85ca2ae24bd87bd5713b
Yooo31 <mrts.yoan@gmail.com>
2023-10-12 23:13:53 +0200

    Add algo to get all data

--- app/components/Technos.tsx
+++ app/components/Technos.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import LanguagesData from "@/data/languages.json";
 
 const Technos = () => {
+  const data = LanguagesData;
+
   return (
     <>
     <div className="flex justify-center m-72">
-      <h2>En construction</h2>
+      { data.map((element, index) => (
+        <div key={index}>
+          <h2>{element.name}</h2>
+          <h2>{element.img}</h2>
+        </div>
+      ))};
     </div>
     </>
   );
