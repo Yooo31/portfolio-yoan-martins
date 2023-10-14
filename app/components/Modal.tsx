227b4f8370280e29b2bb592deeecd4e3015c2e2c
Yooo31 <mrts.yoan@gmail.com>
2023-10-14 15:17:01 +0200

    Projet tab include opup and do styling

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { BiLinkExternal } from 'react-icons/bi';
 
 interface ModalProps {
   ModalOpen: boolean;
@@ -22,12 +24,28 @@ const Modal: React.FC<ModalProps> = ({ ModalOpen, setModalOpen, selectedProject 
 
   return (
     <dialog id="my_modal_3" className={`modal ${ModalOpen ? "modal-open" : ""}`}>
-      <div className="modal-box">
-        <button onClick={() => setModalOpen(false)} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+      <div className="modal-box max-w-full w-1/2">
+        <button onClick={() => setModalOpen(false)} className="btn btn-circle btn-ghost absolute text-4xl right-2 top-2">
           ✕
         </button>
-        <div>
-          <p>Nom du projet: {selectedProject.name}</p>
+        <img src={`/images/projects/${selectedProject.img}`} alt="Shoes" />
+        <div className="card-body">
+          <h2 className="card-title">
+            {selectedProject.name}
+          </h2>
+          <p>{selectedProject.description}</p>
+          <div className="card-actions justify-between mt-5">
+            <div className="justify-start">
+              {selectedProject.technos.map((element, index) => (
+                <div key={index} className="badge badge-outline mr-3">{element}</div>
+              ))}
+            </div>
+            <div className="underline underline-offset-4 decoration-1 hover:decoration-2">
+              {selectedProject.url !== null && (
+                <Link target="_blank" href={selectedProject.url}> <BiLinkExternal className="transition-transform hover:scale-125" cursor="pointer" size={25}/></Link>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </dialog>
--- app/components/Projets.tsx
+++ app/components/Projets.tsx
@@ -17,11 +17,12 @@ const Projets = () => {
     <div className="min-h-full flex flex-wrap place-content-around">
       {data.map((element, index) => (
         <div key={index} className="rounded-xl h-[200px] w-[300px] md:w-[220px] lg:w-[300px] flex flex-col-reverse group">
-          <button onClick={() => openModal(element)}>
-            <div className={`bg-cover bg-center bg-[url(/images/projects/${element.img}.png)] flex flex-col-reverse rounded-xl h-full w-full transition-transform transform scale-100 group-hover:scale-105`}>
-              <h2 className="m-6 text-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-700">
-                {element.img}
-              </h2>
+          <button className="h-full w-full" onClick={() => openModal(element)}>
+            <div className="card w-full h-full bg-base-100 shadow-xl image-full">
+              <figure><img className="group-hover:opacity-25 transition-opacity duration-500" src={`/images/projects/${element.logo}`} alt={element.name} /></figure>
+              <div className="card-body flex-col-reverse">
+                <h2 className="card-title text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">{element.name}</h2>
+              </div>
             </div>
           </button>
         </div>
