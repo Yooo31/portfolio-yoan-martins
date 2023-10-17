e507c65264d48c703bb3bcdfeed27adfb5035f43
SeniorYoan <yoan@kipsoft.fr>
2023-10-17 13:12:28 +0200

    Add logo and pafination

--- app/components/Projets.tsx
+++ app/components/Projets.tsx
@@ -8,14 +8,23 @@ const Projets = () => {
   const [ModalOpen, setModalOpen] = useState<boolean>(false);
   const [selectedProject, setSelectedProject] = useState(null);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 6;
+
   const openModal = (project) => {
     setSelectedProject(project);
     setModalOpen(true);
   };
 
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = data.slice(indexOfFirstProject, indexOfLastProject);
+
+  const totalPages = Math.ceil(data.length / projectsPerPage);
+
   return (
     <div className="min-h-full flex flex-wrap place-content-around">
-      {data.map((element, index) => (
+      {currentProjects.map((element, index) => (
         <div key={index} className="rounded-xl h-[200px] w-[300px] md:w-[220px] lg:w-[300px] flex flex-col-reverse group">
           <button className="h-full w-full" onClick={() => openModal(element)}>
             <div className="card w-full h-full bg-base-100 shadow-xl image-full">
@@ -29,6 +38,12 @@ const Projets = () => {
       ))}
 
       <Modal ModalOpen={ModalOpen} setModalOpen={setModalOpen} selectedProject={selectedProject} />
+
+      <div className="join">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button className={`join-item btn ${currentPage === i + 1 ? "btn-active" : ""}`} key={i} onClick={() => setCurrentPage(i + 1)}>{i + 1}</button>
+        ))}
+      </div>
     </div>
   );
 };
