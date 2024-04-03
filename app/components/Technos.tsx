239a3071be67ae7fffa9ded2f57766591feb8925
Yoan <mrts.yoan@gmail.com>
2024-04-03 14:30:53 +0200

    Add symfony and node js to techno. Hide php and jekyll

--- app/components/Technos.tsx
+++ app/components/Technos.tsx
@@ -20,9 +20,11 @@ const Technos = () => {
     <div id="languages-list" data-aos="zoom-in-up" className="z-40 h-5/6 flex justify-center h-full">
       <div className="flex flex-wrap -mx-4">
         { data.map((element, index) => (
-          <div key={index} className="w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4 flex justify-center">
-            <Image  src={`/images/technos/${element.img}.svg`} alt={element.name} width={150} height={150}/>
-          </div>
+          element.display? (
+            <div key={index} className="w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4 flex justify-center">
+              <Image  src={`/images/technos/${element.img}.svg`} alt={element.name} width={150} height={150}/>
+            </div>
+          ) : null
         ))}
       </div>
     </div>
