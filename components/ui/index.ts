933813c936ab7e6a3b56d356d00dbfd9a80ba2a7
Yoan <mrts.yoan@gmail.com>
2026-02-09 21:50:11 +0100

    feat(RefonteFront): cleaning

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -4,7 +4,5 @@ export type { ButtonProps } from "./button"
 export { Badge, badgeVariants } from "./badge"
 export type { BadgeProps } from "./badge"
 
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card"
-
 export { Input } from "./input"
 export { Textarea } from "./textarea"
